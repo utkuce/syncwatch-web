c5c65b02d4effd96776f44c5d4b15f42e2b919a6
utku <utkuce@gmail.com>
2019-12-05 03:48:52 -0500

    Add firebase room creation

--- peers.js
+++ peers.js
@@ -1,36 +1,57 @@
-var SimplePeer = require('simple-peer')
-var wrtc = require('wrtc')
-
-var peer1 = new SimplePeer({ initiator: true, wrtc: wrtc })
-var peer2 = new SimplePeer({ wrtc: wrtc })
+var firebase = require('firebase/app');
+require('firebase/auth');
+require('firebase/database');
  
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB2sMd7L_8oJnU4e1xaweqBR_UyQlRoxrM",
+    authDomain: "syncwatch-539db.firebaseapp.com",
+    databaseURL: "https://syncwatch-539db.firebaseio.com",
+    projectId: "syncwatch-539db",
+    storageBucket: "syncwatch-539db.appspot.com",
+    messagingSenderId: "917047984813",
+    appId: "1:917047984813:web:c8263931b4178a1f6dbc24"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+var SimplePeer = require('simple-peer');
+var wrtc = require('wrtc');
+
+var peer1 = new SimplePeer({ initiator: true, wrtc: wrtc });
+var peer2 = new SimplePeer({ wrtc: wrtc });
+
+
+var myPeerInfo = {};
+var index = 0;
+
+var uniqid = require('uniqid');
+const uniqueKey = uniqid();
+
 peer1.on('signal', data => {
-  // when peer1 has signaling data, give it to peer2 somehow
-  //peersRef.push({"data": data });
-  
-  peer2.signal(data)
-  //console.log((JSON.stringify(data)));
+      
+    // when peer1 has signaling data, give it to peer2 somehow
+    peer2.signal(data);
 
- // var discordJoin = require('./discordjoin.js');
-  //discordJoin.richPresence(joinSecret=JSON.stringify(data));
-})
- 
+    myPeerInfo[index] = data;
+    index++;
+
+    console.log("Creating firebase entry with key: " + uniqueKey);
+    firebase.database().ref("rooms/" + uniqueKey + "/peer1/").set(myPeerInfo);
+});
+
 peer2.on('signal', data => {
   // when peer2 has signaling data, give it to peer1 somehow
-  peer1.signal(data)
+  peer1.signal(data);
 })
  
 peer1.on('connect', () => {
   // wait for 'connect' event before using the data channel
-  peer1.send('hey peer2, how is it going?')
-})
+  peer1.send('hey peer2, how is it going?');
+});
  
 peer2.on('data', data => {
   // got a data channel message
-  console.log('got a message from peer1: ' + data)
-})
-
-function toBase64(string) {
-
-    return Buffer.from(string).toString('base64');
-}
+  console.log('got a message from peer1: ' + data);
+});
