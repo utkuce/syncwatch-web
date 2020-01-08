23227ca5b560c2511d0094a7a5a8addf7f19baba
utku <utkuce@gmail.com>
2020-01-07 20:22:27 -0500

    Add copying mpv to deploy folder

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
-npx nodegui-packer --init Syncwatch
-npx nodegui-packer --pack ./dist
+npx nodegui-packer --init Syncwatch  && ^
+npx nodegui-packer --pack ./dist  && ^
+copy mpv.exe deploy\win32\build\Syncwatch  && ^
+copy youtube-dl.exe deploy\win32\build\Syncwatch
+
 :: makensis Syncwatch.nsi
